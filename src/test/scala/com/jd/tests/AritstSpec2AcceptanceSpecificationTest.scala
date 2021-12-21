9e2d7f79185695e5344f7fc0c8183249f5ce9ccb
eswaribala <parameswaribala@gmail.com>
2021-12-21 14:51:57 +0530

    Modified

---
+++ src/test/scala/com/jd/tests/AritstSpec2AcceptanceSpecificationTest.scala
@@ -0,0 +1,18 @@
+package com.jd.tests
+
+import com.jd.models.Artist
+import org.specs2.Specification
+
+import java.time.Year
+
+class ArtistSpec2AcceptanceSpecificationTest extends Specification {def is = s2"""
+
+ this is lifetime achievement award specification
+   where age must be greater than 60           $age
+   where awards must be greater than 20        $awards
+                                          """
+  var artist = new Artist("AR","Rahman",100,1954)
+  def age = (Year.now.getValue-artist.birthYear) should be_>(60)
+  def awards = artist.noOfAwards should be_>(20)
+
+}
